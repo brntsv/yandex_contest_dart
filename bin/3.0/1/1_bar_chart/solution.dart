d550897f7a08d7c524b62e61bb6435f3cf0e5873
Nikita Barantsev <nbarentsev@gmail.com>
2023-02-13 21:53:51 +0700

    solve 1

---
+++ bin/3.0/1/1_bar_chart/solution.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+import 'dart:math';
+// https://contest.yandex.ru/contest/45468/problems/
+
+void main() {
+  List<String> lines =
+      File('bin/3.0/1/1_bar_chart/input.txt').readAsLinesSync();
+  List<String> input = [];
+  Map<String, int> map = {};
+  for (var line in lines) {
+    input.addAll(line.split(''));
+  }
+  input
+    ..removeWhere((e) => e == ' ')
+    ..sort();
+  for (int i = 0; input.length > i; i++) {
+    if (map.containsKey(input[i])) {
+      map.update(input[i], (count) => count + 1);
+    } else {
+      map.addEntries({input[i]: 1}.entries);
+    }
+  }
+  int maxStringNumber = map.values.reduce(max);
+  List<int> mapValues = map.values.toList();
+  List<String> output = [];
+  for (int j = maxStringNumber; j > 0; j--) {
+    for (int i = 0; mapValues.length > i; i++) {
+      if (mapValues[i] < j) {
+        output.add(' ');
+      } else {
+        output.add('#');
+      }
+    }
+    print(output.join());
+    output.clear();
+  }
+
+  print(map.keys.join());
+}
