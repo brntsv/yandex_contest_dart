8d8af2711527007ad64884f172c7ec2ddcee3a0c
Nikita Barantsev <nbarentsev@gmail.com>
2023-02-14 22:02:09 +0700

    solve 8

---
+++ bin/3.0/1/8_minimal_rectangle/solution.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'dart:math';
+
+// https://contest.yandex.ru/contest/45468/problems/8/
+
+void main() {
+  List<String> lines =
+      File('bin/3.0/1/8_minimal_rectangle/input.txt').readAsLinesSync();
+  List<String> input = [];
+  for (int i = 1; lines.length > i; i++) {
+    input.addAll(lines[i].split(' '));
+  }
+  List<int> coordinates = input.map((e) => int.parse(e)).toList();
+  List<int> listX = [];
+  List<int> listY = [];
+  for (int i = 0; coordinates.length > i; i++) {
+    if (i.isEven) {
+      listX.add(coordinates[i]);
+    } else {
+      listY.add(coordinates[i]);
+    }
+  }
+  int minX = listX.reduce(min);
+  int minY = listY.reduce(min);
+  int maxX = listX.reduce(max);
+  int maxY = listY.reduce(max);
+
+  print('$minX $minY $maxX $maxY');
+}
