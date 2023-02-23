9df0ec468f6ffdfafcaae42c7e8a55c2bae41803
Nikita Barantsev <nbarentsev@gmail.com>
2023-02-23 23:20:22 +0700

    del uncomplete problems

--- bin/1.0/1/e_emergency/solution.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-// https://contest.yandex.ru/contest/27393/problems/E/
-
-void main() {
-  List<String> lines =
-      File('bin/1.0/1/e_emergency/input.txt').readAsLinesSync();
-  List<String> input = lines[0].split(' ');
-  List<int> inputToInt = input.map((e) => int.parse(e)).toList();
-}
