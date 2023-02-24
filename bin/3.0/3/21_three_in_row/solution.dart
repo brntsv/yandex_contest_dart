4317e5a0d87635e1a8fcda0c6055758b33a25924
Nikita Barantsev <nbarentsev@gmail.com>
2023-02-24 14:10:13 +0700

    solve 21

---
+++ bin/3.0/3/21_three_in_row/solution.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+/* https://contest.yandex.ru/contest/45468/problems/21/
+
+0, 1 = 2
+00, 01, 10, 11 = 4
+000, 001, 010, 011, 100, 101, 110 = 7
+0000, 0001, 0010, 0011, 0100, 0101, 0110, 1000, 1001, 1010, 1011, 1100, 1101 = 13
+*/
+
+void main() {
+  List<String> lines =
+      File('bin/3.0/3/21_three_in_row/input.txt').readAsLinesSync();
+  int n = int.parse(lines[0]);
+  List<int> dp = [2, 4, 7, 13];
+  for (int i = 4; n > i; i++) {
+    dp.add(2 * dp[i - 1] - dp[i - 4]);
+  }
+  print(dp[n - 1]);
+}
